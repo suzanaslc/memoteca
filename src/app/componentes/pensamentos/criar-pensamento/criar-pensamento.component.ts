31722271538fa9b1c34a506fec526206992158d3
suzanaslc <suzanaslc@gmail.com>
2024-03-20 14:29:34 -0300

    craindo o primeiro pensamento estático

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  constructor() {}
+  ngOnInit(): void {
+
+  }
 
 }
